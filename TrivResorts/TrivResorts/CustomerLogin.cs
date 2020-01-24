17386a238c4d65f0887dd300a293f19660cb91df
garytriv <59942593+garytriv@users.noreply.github.com>
2020-01-24 10:52:05 -0800

    Initial Commit-1

---
+++ TrivResorts/TrivResorts/CustomerLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrivResorts
+{
+    class CustomerLogin
+    {
+        public int CustomerNumber { get; }
+        public string CustomerName_First { get; set; }
+        public string CustomerName_Last { get; set; }
+        public string Customer_Login { get; set; }
+        public string CustomerPassword { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
---
+++ TrivResorts/TrivResorts/MgmtLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrivResorts
+{
+    class MgmtLogin
+    {
+        public int MgmtNumber { get; }
+        public string MgmtName_First { get; set; }
+        public string MgmtName_Last { get; set; }
+        public string Mgmt_Login { get; set; }
+        public string MgmtPassword { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
